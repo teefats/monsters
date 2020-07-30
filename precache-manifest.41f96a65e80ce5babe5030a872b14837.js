62966cd296d8599705e32442332f71e32cfd7c3c
teefats <30376635+teefats@users.noreply.github.com>
2020-07-30 08:53:06 +0100

    Updates

---
+++ precache-manifest.41f96a65e80ce5babe5030a872b14837.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2411634eac72a9e1715787e001b3dbc0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0d31558ef826ecb8bebc",
+    "url": "/monsters-rolodex/static/css/main.361b5c97.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/monsters-rolodex/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d31558ef826ecb8bebc",
+    "url": "/monsters-rolodex/static/js/main.37e93471.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
